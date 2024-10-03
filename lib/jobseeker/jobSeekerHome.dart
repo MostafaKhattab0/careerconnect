eb07958da9b06c31dee7d42177d6d7807e7c8a21
Mohamedmostafa234 <mohamedmostafa2342004@gmail.com>
2024-10-03 21:02:52 +0300

    Update jobSeekerHome.dart

--- lib/jobseeker/jobSeekerHome.dart
+++ lib/jobseeker/jobSeekerHome.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
+
 class JobSeekerHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Job Seeker Home",
-        style: TextStyle(
-          fontWeight: FontWeight.w600,
-          color: Colors.cyan
-        ),
+        title: Text(
+          "Job Seeker Home",
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            color: Colors.cyan,
+          ),
         ),
         centerTitle: true,
         backgroundColor: Colors.white70,
+        actions: [
+          IconButton(
+            iconSize: 38,
+            icon: Icon(Icons.person_pin, color: Colors.cyan),
+            onPressed: () {
+              Navigator.pushNamed(context, '/profile');
+            },
+          ),
+        ],
       ),
       body: Stack(
         children: [
@@ -31,76 +42,61 @@ class JobSeekerHome extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-              
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/jobListings');
-                  },
-                  style: ElevatedButton.styleFrom(
-                    elevation: 5,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50),
+                // Job Listings button
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/jobListings');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        elevation: 5,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                        foregroundColor: Colors.deepPurple,
+                        backgroundColor: Colors.white,
+                      ),
+                      child: Text(
+                        "View Job Listings",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.cyan,
+                        ),
+                      ),
                     ),
-                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-                    foregroundColor: Colors.deepPurple,
-                    backgroundColor: Colors.white
-                  ),
-                  child: Text(
-                    "View Job Listings",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.cyan
-                    ),
-                  ),
+                  ],
                 ),
 
-        
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/appliedJobs');
-                  },
-                  style: ElevatedButton.styleFrom(
-                    elevation: 5,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/appliedJobs');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        elevation: 5,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                        foregroundColor: Colors.deepPurple,
+                        backgroundColor: Colors.white,
+                      ),
+                      child: Text(
+                        "View Applied Jobs",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.cyan,
+                        ),
+                      ),
                     ),
-                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-                    foregroundColor: Colors.deepPurple,
-                      backgroundColor: Colors.white
-                  ),
-                  child: Text(
-                    "View Applied Jobs",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                        color: Colors.cyan
-                    ),
-                  ),
-                ),
-
-                
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/profile');
-                  },
-                  style: ElevatedButton.styleFrom(
-                    elevation: 5,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-                    foregroundColor: Colors.deepPurple,
-                      backgroundColor: Colors.white
-                  ),
-                  child: Text(
-                    "View Profile",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                        color: Colors.cyan
-                    ),
-                  ),
+                  ],
                 ),
               ],
             ),
@@ -110,4 +106,3 @@ class JobSeekerHome extends StatelessWidget {
     );
   }
 }
-
