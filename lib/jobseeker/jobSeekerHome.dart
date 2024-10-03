5926dd3ac722dfb90383ac79ff311e137efb92cb
Mohamedmostafa234 <mohamedmostafa2342004@gmail.com>
2024-10-03 19:34:14 +0300

    Update jobSeekerHome.dart

--- lib/jobseeker/jobSeekerHome.dart
+++ lib/jobseeker/jobSeekerHome.dart
@@ -1,35 +1,113 @@
 import 'package:flutter/material.dart';
-
 class JobSeekerHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(centerTitle: true, title: Text("Job Seeker Home")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/jobListings');
-              },
-              child: Text("View Job Listings"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/appliedJobs');
-              },
-              child: Text("View Applied Jobs"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/profile');
-              },
-              child: Text("View Profile"),
-            ),
-          ],
+      appBar: AppBar(
+        title: Text("Job Seeker Home",
+        style: TextStyle(
+          fontWeight: FontWeight.w600,
+          color: Colors.cyan
         ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white70,
+      ),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Container(
+              constraints: BoxConstraints(
+                maxHeight: double.infinity,
+                minHeight: 400,
+              ),
+              child: Image.network(
+                'https://2.bp.blogspot.com/-jGfUbZBDqJg/WZGM8m0EcFI/AAAAAAAAE3U/8zf7JyfebOoBBqdWtQ_W0ODWnwYGxqt4gCLcBGAs/s1600/shutterstock_606904901.jpg',
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+              
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/jobListings');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                    foregroundColor: Colors.deepPurple,
+                    backgroundColor: Colors.white
+                  ),
+                  child: Text(
+                    "View Job Listings",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.cyan
+                    ),
+                  ),
+                ),
+
+        
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/appliedJobs');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                    foregroundColor: Colors.deepPurple,
+                      backgroundColor: Colors.white
+                  ),
+                  child: Text(
+                    "View Applied Jobs",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                        color: Colors.cyan
+                    ),
+                  ),
+                ),
+
+                
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/profile');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                    foregroundColor: Colors.deepPurple,
+                      backgroundColor: Colors.white
+                  ),
+                  child: Text(
+                    "View Profile",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                        color: Colors.cyan
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
