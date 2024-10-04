9f661b0c25fa5ee40d3db15eab3133ea357f29be
Mohamedmostafa234 <mohamedmostafa2342004@gmail.com>
2024-10-04 19:41:50 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,20 @@
-import 'package:careerconnect/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'auth/authSelection.dart';
-import 'auth/hrLogin.dart';
-import 'auth/hrRegistration.dart';
-import 'auth/jobSeekerLogin.dart';
-import 'auth/jobSeekerRegistration.dart';
-import 'hr/hrHome.dart';
-import 'hr/jobAdd.dart';
-import 'hr/jobApplications.dart';
-import 'hr/myJobs.dart';
-import 'hr/viewJobSeekers.dart' as hr;
-import 'hr/viewJobSeekers.dart' as jobseeker;
-import 'jobseeker/jobSeekerHome.dart';
-import 'jobseeker/jobListings.dart';
-import 'jobseeker/appliedJobs.dart';
-import 'jobseeker/profile.dart';
+import 'package:firstp/project/auth/authSelection.dart';
+import 'package:firstp/project/auth/hrLogin.dart';
+import 'package:firstp/project/auth/hrRegistration.dart';
+import 'package:firstp/project/auth/jopSeekerLogin.dart';
+import 'package:firstp/project/auth/jobSeekerRegristration.dart';
+import 'package:firstp/project/hr/hrHome.dart';
+import 'package:firstp/project/hr/jobAdd.dart';
+import 'package:firstp/project/hr/jobApplications.dart';
+import 'package:firstp/project/hr/myJobs.dart';
+import 'package:firstp/project/hr/viewJobSeekers.dart' as hr;
+import 'package:firstp/project/hr/viewJobSeekers.dart' as jobseeker;
+import 'package:firstp/project/jobseeker/jobSeekerHome.dart';
+import 'package:firstp/project/jobseeker/jobListings.dart';
+import 'package:firstp/project/jobseeker/appliedJobs.dart';
+import 'package:firstp/project/jobseeker/profile.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,12 +26,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'CareerConnect App',
       initialRoute: '/',
       routes: {
-        '/': (context) => SplashScreen(),
-        '/authSelection': (context) => AuthSelection(),
+        '/': (context) => AuthSelection(),
         '/hrLogin': (context) => HrLogin(),
         '/hrRegistration': (context) => HrRegistration(),
         '/jobSeekerLogin': (context) => JobSeekerLogin(),
@@ -47,7 +44,8 @@ class MyApp extends StatelessWidget {
         '/jobListings': (context) => JobListings(),
         '/appliedJobs': (context) => AppliedJobs(),
         '/profile': (context) => ProfilePage(),
-      },
+        'hrHome': (context) => AuthSelection(),
+        '/authSelection': (context) => AuthSelection(),      },
     );
   }
 }
