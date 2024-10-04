d2b3f34d307c2b2a2bf7cc5b75da7c320b483055
Mohamedmostafa234 <mohamedmostafa2342004@gmail.com>
2024-10-04 17:54:20 +0300

    Update jobSeekerRegistration.dart

--- lib/auth/jobSeekerRegistration.dart
+++ lib/auth/jobSeekerRegistration.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import '../jobSeeker/jobSeekerHome.dart';
+import 'package:firstp/project/jobseeker/jobSeekerHome.dart';
 
 class JobSeekerRegistration extends StatefulWidget {
   @override
@@ -12,42 +10,12 @@ class JobSeekerRegistration extends StatefulWidget {
 }
 
 class _JobSeekerRegistrationState extends State<JobSeekerRegistration> {
-  final ImagePicker _picker = ImagePicker();
-  XFile? _imageFile;
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _phoneController = TextEditingController();
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  final FirebaseStorage _storage = FirebaseStorage.instance;
-
-  Future<void> _pickImage() async {
-    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
-    setState(() {
-      _imageFile = pickedFile;
-    });
-  }
-
-  Future<String?> _uploadImageToStorage() async {
-    if (_imageFile == null) return null;
-
-    try {
-
-      String fileName = '${DateTime.now().millisecondsSinceEpoch}_${_imageFile!.name}';
-      Reference reference = _storage.ref().child('seekerImages/$fileName');
-
-
-      await reference.putFile(File(_imageFile!.path));
-
-
-      String downloadUrl = await reference.getDownloadURL();
-      return downloadUrl;
-    } catch (e) {
-      print('Image upload failed: $e');
-      return null;
-    }
-  }
 
   Future<void> _register() async {
     try {
@@ -58,33 +26,24 @@ class _JobSeekerRegistrationState extends State<JobSeekerRegistration> {
         throw Exception("All fields must be filled.");
       }
 
-
       UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
         email: _emailController.text.trim(),
         password: _passwordController.text.trim(),
       );
-
-
-      String? imageUrl = await _uploadImageToStorage();
-
 
       Map<String, dynamic> userData = {
         'name': _nameController.text.trim(),
         'email': _emailController.text.trim(),
         'role': 'job seeker',
         'phone': _phoneController.text.trim(),
-        'profileImage': imageUrl,
       };
 
-
       await _firestore.collection('users').doc(userCredential.user!.uid).set(userData);
-
 
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (context) => JobSeekerHome()),
       );
     } catch (e) {
-
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Registration failed: $e')),
       );
@@ -94,46 +53,65 @@ class _JobSeekerRegistrationState extends State<JobSeekerRegistration> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Job Seeker Registration',
-        style: TextStyle(
+      appBar: AppBar(title: Text('Job Seeker Registration',style:
+      TextStyle(
           fontWeight: FontWeight.w600,
-          color: Colors.cyan,
-        ),
-      )),
+          color: Colors.white,
+          fontSize: 20
+      ),
+      ),
+      backgroundColor: Colors.cyan,),
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
           children: [
+            SizedBox(height: 16),
             TextField(
               controller: _nameController,
-              decoration: InputDecoration(labelText: 'Name'),
+              decoration: InputDecoration(labelText: 'Name',labelStyle:
+              TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.cyan,
+                  fontSize: 20
+              ),),
             ),
             TextField(
               controller: _emailController,
-              decoration: InputDecoration(labelText: 'Email'),
+              decoration: InputDecoration(labelText: 'Email',labelStyle:
+              TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.cyan,
+                  fontSize: 20
+              ),),
             ),
             TextField(
               controller: _passwordController,
-              decoration: InputDecoration(labelText: 'Password'),
+              decoration: InputDecoration(labelText: 'Password',labelStyle:
+              TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.cyan,
+                  fontSize: 20
+              ),),
               obscureText: true,
             ),
             TextField(
               controller: _phoneController,
-              decoration: InputDecoration(labelText: 'Phone'),
-            ),
-            SizedBox(height: 20),
-            // Display the selected image
-            if (_imageFile != null)
-              Image.file(File(_imageFile!.path), height: 100, width: 100),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: _pickImage,
-              child: Text('Select Image'),
+              decoration: InputDecoration(labelText: 'Phone',labelStyle:
+              TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.cyan,
+                  fontSize: 20
+              ),),
             ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: _register,
-              child: Text('Register'),
+              child: Text('Register',style:
+              TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.cyan,
+                  fontSize: 20
+              ),),
             ),
           ],
         ),
