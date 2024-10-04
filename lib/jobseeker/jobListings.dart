27cdfc9e9fe575b936a436a84465f019aaa64ff4
Mohamedmostafa234 <mohamedmostafa2342004@gmail.com>
2024-10-04 18:28:06 +0300

    Update jobListings.dart

--- lib/jobseeker/jobListings.dart
+++ lib/jobseeker/jobListings.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'job_detail.dart'; // Import the JobDetail page
+import 'job_detail.dart';
 
 class JobListings extends StatefulWidget {
   @override
