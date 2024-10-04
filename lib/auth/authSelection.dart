0f621a0e745843d8a83b3b74fcee9806e54eac66
Mohamedmostafa234 <mohamedmostafa2342004@gmail.com>
2024-10-04 17:29:41 +0300

    Update authSelection.dart

--- lib/auth/authSelection.dart
+++ lib/auth/authSelection.dart
@@ -6,59 +6,92 @@ class AuthSelection extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text("Select Role",
+        title: Text(
+          "Select Role",
           style: TextStyle(
             fontWeight: FontWeight.w600,
-            color: Colors.cyan,
-          ),),
-      ),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                fixedSize: Size(150, 150),
-                backgroundColor: Colors.red,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-              ),
-              onPressed: () {
-                Navigator.pushNamed(context, '/jobSeekerLogin');
-              },
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.account_circle, size: 50, color: Colors.white),
-                  SizedBox(height: 10),
-                  Text('Job Seeker', style: TextStyle(fontSize: 18, color: Colors.white)),
-                ],
-              ),
-            ),
-            SizedBox(width: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                fixedSize: Size(150, 150),
-                backgroundColor: Colors.blue,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-              ),
-              onPressed: () {
-                Navigator.pushNamed(context, '/hrLogin');
-              },
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.gavel, size: 50, color: Colors.white),
-                  SizedBox(height: 10),
-                  Text('HR', style: TextStyle(fontSize: 18, color: Colors.white)),
-                ],
-              ),
-            ),
-          ],
+            color: Colors.white,
+            fontSize: 22,
+          ),
         ),
+        backgroundColor: Colors.cyan,
+      ),
+      body: Stack(
+        children: [
+          Positioned(
+            top: kToolbarHeight,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 200,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                    'https://play-lh.googleusercontent.com/fefr3LainFJXc-mMyvLYutuSSrpFmJMzUdeqgGYs37DRoUhfcUgkPnAjGEOLoLArYh4',
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: EdgeInsets.only(top: 75),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: Size(150, 150),
+                      backgroundColor: Colors.white70,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/jobSeekerLogin');
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.account_circle,
+                            size: 50, color:
+                            Colors.cyan),
+                        SizedBox(height: 10),
+                        Text('Job Seeker',
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.cyan)
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: Size(150, 150),
+                      backgroundColor: Colors.cyan,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/hrLogin');
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.gavel, size: 50, color: Colors.white),
+                        SizedBox(height: 10),
+                        Text('HR', style: TextStyle(fontSize: 18, color: Colors.white)),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
