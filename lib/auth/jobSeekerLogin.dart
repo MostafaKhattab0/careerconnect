3ed65b4ef3c95c8e718a0462512e8247713dd02c
Mohamedmostafa234 <mohamedmostafa2342004@gmail.com>
2024-10-04 17:49:42 +0300

    Update jobSeekerLogin.dart

--- lib/auth/jobSeekerLogin.dart
+++ lib/auth/jobSeekerLogin.dart
@@ -20,15 +20,14 @@ class _JobSeekerLoginState extends State<JobSeekerLogin> {
         password: passwordController.text.trim(),
       );
 
-
       DocumentSnapshot userDoc = await _firestore.collection('users').doc(userCredential.user!.uid).get();
       if (userDoc.exists && userDoc['role'] == 'job seeker') {
-
         Navigator.pushNamed(context, '/jobSeekerHome');
       } else {
-
         await _auth.signOut();
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Unauthorized access. Please log in as a job seeker.")));
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Unauthorized access. Please log in as a job seeker.")),
+        );
       }
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(e.toString())));
@@ -39,27 +38,62 @@ class _JobSeekerLoginState extends State<JobSeekerLogin> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Job Seeker Login',
+        title: Text(
+          'Job Seeker Login',
           style: TextStyle(
             fontWeight: FontWeight.w600,
-            color: Colors.cyan,
-          ),),
+            color: Colors.white,
+            fontSize: 22,
+          ),
+        ),
+        backgroundColor: Colors.cyan,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            TextFormField(controller: emailController, decoration: InputDecoration(labelText: 'Email')),
+            SizedBox(height: 40),
+            Image.network(
+              'https://thumbs.dreamstime.com/b/abstract-job-search-background-vector-illustration-design-abstract-job-search-background-vector-illustration-design-banners-251243471.jpg',
+              fit: BoxFit.contain,
+            ),
             SizedBox(height: 16),
-            TextFormField(controller: passwordController, decoration: InputDecoration(labelText: 'Password'), obscureText: true),
+            TextFormField(
+              controller: emailController,
+              decoration: InputDecoration(labelText: 'Email',labelStyle:
+              TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.cyan,
+                  fontSize: 20
+              ),),
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              controller: passwordController,
+              decoration: InputDecoration(labelText: 'Password',labelStyle:
+              TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.cyan,
+                  fontSize: 20
+              ),),
+              obscureText: true,
+            ),
             SizedBox(height: 32),
-            ElevatedButton(onPressed: loginJobSeeker, child: Text('Login')),
+            ElevatedButton(
+              onPressed: loginJobSeeker,
+              child: Text('Login',style:
+              TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.cyan,
+                  fontSize: 20
+              ),),
+            ),
             SizedBox(height: 16),
             TextButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/jobSeekerRegistration');
               },
-              child: Text('Register as Job Seeker', style: TextStyle(color: Colors.blue)),
+              child: Text('Register as Job Seeker', style: TextStyle(color: Colors.cyan)),
             ),
           ],
         ),
